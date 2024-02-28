a188b89abaf24046879d67ce38c7be9ccf9b13cd
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-02-29 03:21:50 +0600

    added nav bar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        clifford: "#da373d",
+        "Dark-1": "#100F0F",
+        "Dark-2": "#403F3F",
+        "Dark-3": "#706F6F",
+        "primary-blue": "#0D6EFD",
       },
     },
   },
