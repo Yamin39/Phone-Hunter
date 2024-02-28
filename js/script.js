ead70e7eebbf8429800d4b678d3e593e216271e1
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-02-29 04:49:47 +0600

    added products section and related logics

--- js/script.js
+++ js/script.js
@@ -11,3 +11,45 @@ tailwind.config = {
     },
   },
 };
+
+// getting data by api
+
+async function loadPhone() {
+  const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+  const data = await res.json();
+  const phones = data.data;
+  console.log(data, phones);
+  displayPhone(phones);
+}
+
+loadPhone();
+
+// displaying phones
+
+function displayPhone(phones) {
+  const phoneContainer = document.getElementById("product-container");
+  phones.forEach((phone) => {
+    const div = document.createElement("div");
+    div.classList = "card border border-[#CFCFCF]";
+    div.innerHTML = `
+    <figure class="px-10 pt-10">
+        <img src="${phone.image}" alt="Phone" class="rounded-xl" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title font-bold text-2xl text-dark-2">
+            ${phone.phone_name}
+        </h2>
+        <p class="text-dark-3 text-lg">
+            There are many variations of passages of available, but the majority have suffered
+        </p>
+        <span class="text-dark-2 text-2xl font-bold">
+            $999
+        </span>
+        <div class="card-actions">
+            <button class="btn bg-primary-blue text-white hover:bg-[#4a92ff] py-2 px-6 h-auto font-semibold text-xl">Show Details</button>
+        </div>
+    </div>
+    `;
+    phoneContainer.appendChild(div);
+  });
+}
