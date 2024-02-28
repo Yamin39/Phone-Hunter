5a944f40e909c640a8206447b690747e119afef5
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-02-29 05:34:31 +0600

    footer added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        "Dark-1": "#100F0F",
-        "Dark-2": "#403F3F",
-        "Dark-3": "#706F6F",
+        "dark-1": "#100F0F",
+        "dark-2": "#403F3F",
+        "dark-3": "#706F6F",
         "primary-blue": "#0D6EFD",
       },
     },
